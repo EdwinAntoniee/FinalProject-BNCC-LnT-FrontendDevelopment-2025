ba403e628d88559d2f51adb4248b7e7703a3e14c
Edwin <edwin.xw23@gmail.com>
2025-06-07 03:16:59 +0700

    Link Product-page

---
+++ checkout-page/js/shopping-cart.js
@@ -0,0 +1,17 @@
+const checkoutBtn = document.querySelector('.summary__checkout');
+const modal = document.getElementById('checkoutModal');
+const closeModalBtn = document.getElementById('closeModal');
+
+checkoutBtn.addEventListener('click', () => {
+  modal.classList.add('modal--active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.remove('modal--active');
+});
+
+modal.querySelector('.modal__overlay').addEventListener('click', () => {
+  modal.classList.remove('modal--active');
+});
+
+
